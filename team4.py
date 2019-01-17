06e96faebd2688a14b6b3c87bd5c009474c87ed9
Barryck-H <46536473+Barryck-H@users.noreply.github.com>
2019-01-17 12:12:14 -0600

    Update team4.py

--- team4.py
+++ team4.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'ReallyStupid' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'SnitchesGetRitches'
+strategy_description = ''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
